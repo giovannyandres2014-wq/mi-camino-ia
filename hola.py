4fcd0deb656e46785e6469a9af9e05eae0c71b3d
Giovanny Andres <giovannyandres2014@gmail.com>
2026-09-12 17:46:14 -0500

    cambio de edades

--- hola.py
+++ hola.py
@@ -1,9 +1,11 @@
 print("Hola, voy a ser AI Agent Engineer")
 print("Estoy aprendiendo git hub")
 nombre = "GIO"
-edad = 30
+edad = 15
 es_programador = True
 
 print("Me llamo" , nombre)
 print("Mi edad es" , edad)
 print("Soy Programador?" , es_programador)
+if edad >= 18: print("Eres mayor de edad")
+else:print("Eres Menor de Edad")
