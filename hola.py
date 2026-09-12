fbd802a30dafa7d50b8ce32bc485c7a694ecaf67
Giovanny Andres <giovannyandres2014@gmail.com>
2026-09-12 17:32:00 -0500

    ejercicio variables y tipos de datos

--- hola.py
+++ hola.py
@@ -1,2 +1,9 @@
 print("Hola, voy a ser AI Agent Engineer")
 print("Estoy aprendiendo git hub")
+nombre = "GIO"
+edad = 30
+es_programador = True
+
+print("Me llamo" , nombre)
+print("Mi edad es" , edad)
+print("Soy Programador?" , es_programador)
