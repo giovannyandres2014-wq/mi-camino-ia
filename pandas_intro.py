acbc9f345ce1969fa0fde6576e60d269ecec95a6
Giovanny Andres <giovannyandres2014@gmail.com>
2026-09-14 10:33:54 -0500

    columna calculada con apply

--- pandas_intro.py
+++ pandas_intro.py
@@ -15,3 +15,20 @@ print("Total de unidades en inventario:", total)
 agotados = datos[datos["Cantidad"] == 0]
 print("Productos agotados:")
 print(agotados)
+
+# ============================================
+# Crear una columna nueva con una condición (como un SI de Excel) Cuando después usas .apply(estado_producto), 
+# pandas toma esa función y la va llamando automáticamente para cada fila de tu columna "Cantidad", una por una, 
+# como si tú mismo llamaras la función 4 veces seguidas (una por cada producto), 
+# pero sin tener que escribir esas 4 llamadas a mano.
+# ============================================
+
+def estado_producto(cantidad):
+    if cantidad == 0:
+        return "Agotado"
+    else:
+        return "Disponible"
+
+datos["Estado"] = datos["Cantidad"].apply(estado_producto)
+
+print(datos)
