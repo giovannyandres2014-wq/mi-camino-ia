0f9a92737931d5598f20b7dab17ddd7d20451890
Giovanny Andres <giovannyandres2014@gmail.com>
2026-09-14 11:58:41 -0500

    api con parametros clima

--- api_intro.py
+++ api_intro.py
@@ -21,3 +21,23 @@ datos_json = respuesta.json()
 
 # Ahora podemos acceder a un valor especifico, como si fuera una cajita con nombre
 print("URL de usuarios:", datos_json["user_url"])
+
+# ============================================
+# Enviar parametros a una API
+# ============================================
+
+parametros = {
+    "latitude": 6.25,
+    "longitude": -75.56,
+    "current_weather": True
+}
+
+respuesta_clima = requests.get("https://api.open-meteo.com/v1/forecast", params=parametros)
+datos_clima = respuesta_clima.json()
+
+print(datos_clima)
+
+clima_actual = datos_clima["current_weather"]
+
+print("Temperatura actual:", clima_actual["temperature"], "grados")
+print("Velocidad del viento:", clima_actual["windspeed"], "km/h")
